eb1bd25b540bcaaeaf34b5c225f70551825592c6
José Muñoz R <jose.munoz@senegocia.com>
2017-04-06 12:50:39 -0300

    git init base for cli ngcomponent

---
+++ app/app.js
@@ -0,0 +1,6 @@
+import fs from 'fs' 
+
+fs.writeFile('message.txt', 'Hello Node.js', (err) => {
+  if (err) throw err;
+  console.log('It\'s saved!');
+});
---
+++ index.js
@@ -0,0 +1,12 @@
+'use strict';
+
+var _fs = require('fs');
+
+var _fs2 = _interopRequireDefault(_fs);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+_fs2.default.writeFile('message.txt', 'Hello Node.js', function (err) {
+  if (err) throw err;
+  console.log('It\'s saved!');
+});
